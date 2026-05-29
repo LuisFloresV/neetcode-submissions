4288d14bfba39910bcc032fe996aefe98418b43e
Luis Flores <65628587+LuisFloresV@users.noreply.github.com>
2026-05-29 10:10:24 -0600

    Add: reverse-a-linked-list - submission-9

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-9.ts
@@ -0,0 +1,15 @@
+class Solution {
+    reverseList(head: ListNode | null): ListNode | null {
+        let prev = null;
+        let current = head;
+
+        while (current) {
+            const next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
